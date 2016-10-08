38dc13f386e626345b281b9f2cf5d697f201c751
razze <razze@RAZZEL-PC>
2016-10-08 16:00:16 +0800

    parsing&error2

--- PayRollGUI/Form1.cs
+++ PayRollGUI/Form1.cs
@@ -23,8 +23,38 @@ namespace PayRollGUI
             double DailyRate, BasicPay, OvertimePay, SSS, Pagibig, PhilHealth, Loan, Tax, AbsDed, GrossPay, TotalDed, NetPay;
             int HrsWork, NumAbs, LoanYrs;
             char LoanCode;
-            //asd
-            //dsad
+            int errcode = 0;
+            /*ERROR CODES
+             * 1 = Invalid Input
+             */
+
+            if(double.TryParse(txtDailyRate.Text, out DailyRate) && 
+               int.TryParse(txtHrsWork.Text, out HrsWork) &&
+               int.TryParse(txtNumAbs.Text, out NumAbs) &&
+               char.TryParse(cmbLoanCode.Text, out LoanCode) &&
+               int.TryParse(cmbLoanYrs.Text, out LoanYrs) &&
+               double.TryParse(txtSSS.Text, out SSS) &&
+               double.TryParse(txtPagibig.Text, out Pagibig) &&
+               double.TryParse(txtPhilHealth.Text, out PhilHealth) &&
+               txtEmpName.Text.Length > 0 &&
+               txtEmpCode.Text.Length > 0)
+            {
+                EmpName = txtEmpName.Text;
+                EmpCode = txtEmpCode.Text;
+                DailyRate = double.Parse(txtDailyRate.Text);
+                HrsWork = int.Parse(txtHrsWork.Text);
+                NumAbs= int.Parse(txtNumAbs.Text);
+                LoanCode = char.Parse(cmbLoanCode.Text);
+                LoanYrs = int.Parse(cmbLoanYrs.Text);
+                SSS = double.Parse(txtSSS.Text);
+                Pagibig = double.Parse(txtPagibig.Text);
+                PhilHealth = double.Parse(txtPhilHealth.Text);
+            }
+            else
+            {
+                errcode = 1;
+            }
+
         }
     }
 }
